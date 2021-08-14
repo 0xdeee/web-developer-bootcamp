e5743eb81fef967c8630a2c06a9a5caa04d09358
0xdeee <21086243+0xdeee@users.noreply.github.com>
2021-08-14 15:49:34 +0530

    completed REST Intro section

--- JavaScript/02.node-Intro/06.REST-intro/index.js
+++ JavaScript/02.node-Intro/06.REST-intro/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const redditData = require('./data.json');
+const { v4: uuid } = require('uuid');
+const methodOverride = require('method-override');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, '/public')));
-// middleware fn to add support to parse different ttype of data in to req body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -15,12 +16,66 @@ app.listen(8080, () =>
   console.log('server started at 8080 and listenting to incoming requests')
 );
 
-app.get('/', (req, res) => {
-  const name = 'Home';
-  res.render('home', { name });
+const comments = [
+  {
+    id: uuid(),
+    username: 'qwerty',
+    comment: '1qaz2wsx',
+  },
+  {
+    id: uuid(),
+    username: 'asdfg',
+    comment: '3edc4rfv',
+  },
+  {
+    id: uuid(),
+    username: 'zxcvbn',
+    comment: '5tgb6yhn',
+  },
+];
+
+app.get('/comments', (req, res) => {
+  res.render('index', { comments });
 });
 
-app.post('/buy', (req, res) => {
-  console.dir(req.body);
-  res.send('success');
+app.get('/comments/new', (req, res) => {
+  res.render('new');
+});
+
+app.post('/comments', (req, res) => {
+  // console.log(req.body);
+  req.body.id = uuid();
+  comments.push(req.body);
+  res.redirect('/comments');
+});
+
+app.get('/comments/:id', (req, res) => {
+  const { id } = req.params;
+  const selectedComment = comments.find((comment) => comment.id === id);
+  // console.log(id);
+  // console.log(selectedComment);
+  res.render('show', { selectedComment });
+});
+
+app.get('/comments/:id/edit', (req, res) => {
+  const { id } = req.params;
+  const selectedComment = comments.find((comment) => comment.id === id);
+  res.render('edit', { selectedComment });
+});
+
+app.patch('/comments/:id', (req, res) => {
+  const { id } = req.params;
+  const editedComment = req.body.comment;
+  const selectedComment = comments.find((comment) => comment.id === id);
+  selectedComment.comment = editedComment;
+  res.redirect('/comments');
+});
+
+app.delete('/comments/:id', (req, res) => {
+  const { id } = req.params;
+  const selectedCommentIndex = comments.findIndex(
+    (comment) => comment.id === id
+  );
+  comments.splice(selectedCommentIndex, 1);
+  res.redirect('/comments');
 });
